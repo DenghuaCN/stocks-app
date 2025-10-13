305652d062f003615812501f05f95a85d4e4c1b7
denghua <denghua.fm@foxmail.com>
2025-10-13 17:44:17 +0800

    feat: implement header.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Signalist",
-  description: "Track real-time stock prices, get personlized stock alerts and expolre details company insights.",
+  description: "Track real-time stock prices, get personalized stock alerts and explore details company insights.",
 };
 
 export default function RootLayout({
